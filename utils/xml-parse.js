ea06abceb20085802f293a1013788a9d53c248dc
Loraine Kanervisto <loraine@lorainekv.com>
2021-10-10 19:41:13 -0700

    Build a station to timestamp mapping

---
+++ utils/xml-parse.js
@@ -0,0 +1,31 @@
+var parseString = require('xml2js').parseString;
+var fs = require('fs');
+
+var stationNameList = [
+  "erkner",
+  "hauptbahnhof",
+  "rathaus-steglitz",
+  "tegel",
+  "westkreuz",
+  "wittenau",
+  "zoo-garten",
+]
+
+var stationToTimesMap = {};
+
+stationNameList.forEach(function (stationName, idx) {
+  var xmlString = fs.readFileSync(`./station-timetables/formatted/${stationName}.xml`).toString('utf-8');
+  parseString(xmlString, function (err, result) {
+    var trains = result.timetable.s;
+
+    stationToTimesMap[stationName] = [];
+    trains.forEach(function (train, idx) {
+      if (train.ar) {
+        var timestamp = train.ar[0].$.pt;
+        stationToTimesMap[stationName].push(timestamp);
+      }
+    });
+  });
+});
+
+console.log(stationToTimesMap);
